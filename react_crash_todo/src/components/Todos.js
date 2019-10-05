6852f6d6c488a77e5fb19a43be6c653fc8b32072
JOHNSON JOSEPH <johnsonjoseph@JOHNSONs-MacBook-Air.local>
2019-10-05 15:44:24 -0700

    created todoitem.js file to create a item component to use in my todo.js file

--- react_crash_todo/src/components/Todos.js
+++ react_crash_todo/src/components/Todos.js
@@ -4,11 +4,10 @@ class Todos extends Component {
 //function Todos() 
 render(){
     console.log(this.props.todos)
-  return (
-    <div className="App">
-      <h1> Todos</h1>
-    </div>
-  );
+  return this.props.todos.map((todo) => (
+        <h3>{todo.title}</h3>
+
+  ));
  }
 }
 
