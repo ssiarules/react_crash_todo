5d3dfd8bd6d46c139b6409b03702248b6c8123b7
JOHNSON JOSEPH <johnsonjoseph@JOHNSONs-MacBook-Air.local>
2019-10-05 16:05:53 -0700

    defined proptypes for our todos.js, why use proptypes and the reason we use keys when mapping our data

--- react_crash_todo/src/components/Todos.js
+++ react_crash_todo/src/components/Todos.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types'; //proptypes is a validation for properties that a component should have. We can set the type and set to be required or not. 
 
 class Todos extends Component {
 //function Todos() 
 render(){
     console.log(this.props.todos)
-  return this.props.todos.map((todo) => (
-        <h3>{todo.title}</h3>
+  return this.props.todos.map((todo) => ( // when we map through something it creates a list which should have unique keys. We have access to Todo which is unquie and has an id. 
+       < TodoItem key={todo.id} todo={todo}/>
 
   ));
  }
 }
+
+Todos.propTypes = {
+    todos: PropTypes.object.isRequired 
+}
+
 
 export default Todos;
